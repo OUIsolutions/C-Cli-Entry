070fbdb6fcc8940a661322acee531371f6744d32
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-07-28 23:38:59 -0300

    att

--- CLinEntry/Entry/Entry.h
+++ CLinEntry/Entry/Entry.h
@@ -3,10 +3,15 @@
 typedef struct CliEntry{
     CliStringArray * elements;
     char *flag_identifiers;
-    bool case_sensitive;
+    bool case_sensitive_for_flags;
+    bool case_sensitive_for_args;
+
 
     CliStringArray * (*get_args)(CliStringArray *self, char *flags);
-
+    char *  (*get_arg)(CliStringArray *self, char *flags);
+    long  (*get_long)(CliStringArray *self, char *flags);
+    double  (*get_double)(CliStringArray *self, char *flags);
+    bool (*exist)(CliStringArray *self, char *flags);
 
 }CliEntry;
 
