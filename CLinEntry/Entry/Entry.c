74dff0dc9cf1dd8515e6069135d710f96cb2d0ca
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-05 22:25:40 -0300

    att

---
+++ CLinEntry/Entry/Entry.c
@@ -0,0 +1,28 @@
+
+
+CliEntry * newCliEntry(int argc, char **argv){
+    CliEntry *self = (CliEntry*) malloc(sizeof (CliEntry));
+    self->elements = newCTextArray();
+    self->flag_identifiers = "-,--,---";
+    self->get_flag = CliEntry_get_flag;
+    self->get_str = CliEntry_get_str;
+    self->get_long = CliEntry_get_long;
+    self->get_double = CliEntry_get_double;
+    self->get_bool = CliEntry_get_bool;
+
+    for(int i = 0; i < argc; i++){
+        CTextArray_append_string(self->elements,argv[i]);
+    }
+
+}
+
+CliFlag *CliEntry_get_flag(CliEntry *self,const char *flags,bool case_sensitive){
+
+}
+char*   CliEntry_get_str(CliEntry *self, int position, bool case_sensitive);
+
+long    CliEntry_get_long(CliEntry *self, int position);
+double  CliEntry_get_double(CliEntry *self, int position);
+double  CliEntry_get_bool(CliEntry *self, int position);
+void CliEntry_free(struct CliEntry *self);
+
--- CLinEntry/Entry/Entry.h
+++ CLinEntry/Entry/Entry.h
@@ -8,24 +8,26 @@ typedef struct CliEntry{
     CTextArray * elements;
     char *flag_identifiers;
 
+    
 
     CliFlag *(*get_flag)(struct CliEntry *self,const char *flags,bool case_sensitive);
-    char*   (*get_arg)(struct CliEntry *self, int position,bool case_sensitive);
+    char*   (*get_str)(struct CliEntry *self, int position, bool case_sensitive);
 
     long    (*get_long)(struct CliEntry *self, int position);
     double  (*get_double)(struct CliEntry *self, int position);
     double  (*get_bool)(struct CliEntry *self, int position);
-
+    void (*free)(struct CliEntry *self);
 
 }CliEntry;
 
 CliEntry * newCliEntry(int argc, char **argv);
 
 CliFlag *CliEntry_get_flag(CliEntry *self,const char *flags,bool case_sensitive);
-char*   CliEntry_get_arg(CliEntry *self, int position,bool case_sensitive);
+char*   CliEntry_get_str(CliEntry *self, int position, bool case_sensitive);
 
 long    CliEntry_get_long(CliEntry *self, int position);
 double  CliEntry_get_double(CliEntry *self, int position);
 double  CliEntry_get_bool(CliEntry *self, int position);
+void CliEntry_free(struct CliEntry *self);
 
 
