278da2878fc27c5181ab804071eae0b1cce0444e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-06 23:49:08 -0300

    att

--- CLinEntry/Entry/Entry.c
+++ CLinEntry/Entry/Entry.c
@@ -18,13 +18,29 @@ CliFlag *CliEntry_get_flag(CliEntry *self,const char *flags,bool case_sensitive)
 
 }
 char*   CliEntry_get_str(CliEntry *self, int position, bool case_sensitive){
-        
+        long converted_position = private_CText_transform_index(self->elements->size,position);
+        CTextStack *current = self->elements->stacks[converted_position];
+        char *result = strdup(current->rendered_text);
+        private_cli_append_gargabe(self->garbage_array,PRIVATE_CLI_CHAR_TRASH,result);
+        return result;
 }
 
-long    CliEntry_get_long(CliEntry *self, int position);
-double  CliEntry_get_double(CliEntry *self, int position);
-double  CliEntry_get_bool(CliEntry *self, int position);
+long    CliEntry_get_long(CliEntry *self, int position){
+    long converted_position = private_CText_transform_index(self->elements->size,position);
+    CTextStack *current = self->elements->stacks[converted_position];
+    return CTextStack_parse_to_integer(current);
+}
+double  CliEntry_get_double(CliEntry *self, int position){
+    long converted_position = private_CText_transform_index(self->elements->size,position);
+    CTextStack *current = self->elements->stacks[converted_position];
+    return CTextStack_parse_to_double(current);
+}
 
+double  CliEntry_get_bool(CliEntry *self, int position){
+    long converted_position = private_CText_transform_index(self->elements->size,position);
+    CTextStack *current = self->elements->stacks[converted_position];
+    return CTextStack_parse_to_bool(current);
+}
 
 void CliEntry_free(struct CliEntry *self){
     CTextArray_free(self->elements);
