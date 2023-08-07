07dd20321a47a994ed4b907611ac3821209bc31f
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-07 02:42:07 -0300

    at

--- CLinEntry/Extra/Extra.c
+++ CLinEntry/Extra/Extra.c
@@ -1,15 +1,19 @@
-
+void private_cli_parse_flags_sort_text_array(CTextArray *element){
+    
+}
 
 CTextArray * private_cli_parse_flags(const char *flags,bool case_sensitive){
     CTextArray *elements = CTextArray_split(flags,"|");
     CTextArray_foreach(elements,CTextStack_self_trim);
+
     if(!case_sensitive){
         CTextArray_foreach(elements,CTextStack_self_lower);
     }
+
     return elements;
 }
 
-CTextStack *private_cli_get_flag_if_its_an_flag(CTextArray *identifiers,CTextStack *possible_flag,bool case_sensitve){
+CTextStack * private_cli_get_flag_if_its_an_flag(CTextArray *identifiers,CTextStack *possible_flag,bool case_sensitve){
 
     CTextStack *formated_possible_flag = CTextStack_clone(possible_flag);
     if(!case_sensitve){
