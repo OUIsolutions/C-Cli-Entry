bf82fc43a91305012aa9865c80f3d05bd7422f5a
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-07 05:49:21 -0300

    att

--- main.c
+++ main.c
@@ -5,8 +5,12 @@
 int main(int argc, char **argv){
     CliEntry *e = newCliEntry(argc,argv);
 
-    CliEntry_get_flag(e,"help | h ",CLI_NOT_CASE_SENSITIVE);
+    CliFlag *f = CliEntry_get_flag(e,"help | h ",CLI_NOT_CASE_SENSITIVE);
+    if(f){
+        CTextArray_represent(f->elements);
+
+    }
     CliEntry_free(e);
-    
+
     
 }
