992b32eac406063d01a954d4b253598ca9b54361
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-07 02:19:26 -0300

    att

--- CLinEntry/Extra/Extra.c
+++ CLinEntry/Extra/Extra.c
@@ -11,25 +11,23 @@ CTextArray * private_cli_parse_flags(const char *flags,bool case_sensitive){
 
 CTextStack *private_cli_get_flag_if_its_an_flag(CTextArray *identifiers,CTextStack *possible_flag,bool case_sensitve){
 
-    CTextStack *formated_possible_flag = possible_flag;
+    CTextStack *formated_possible_flag = CTextStack_clone(possible_flag);
     if(!case_sensitve){
-        formated_possible_flag = CTextStack_lower(possible_flag);
+        CTextStack_self_lower(formated_possible_flag);
     }
 
-    for(int i =0 ; i < identifiers->size; i++){
+
+    for(int i =0; i < identifiers->size; i++){
 
         CTextStack  *current_identifier = identifiers->stacks[i];
 
         if(CTextStack_starts_with(formated_possible_flag,current_identifier->rendered_text)){
-            if(!case_sensitve){
-                CTextStack_free(formated_possible_flag);
-            }
-            return CTextStack_substr(formated_possible_flag, (long)current_identifier->size,-1);
+            CTextStack_self_substr(formated_possible_flag, (long)current_identifier->size,-1);
+            return formated_possible_flag;
         }
 
     }
-    if(!case_sensitve){
-        CTextStack_free(formated_possible_flag);
-    }
+    CTextStack_free(formated_possible_flag);
+
     return NULL;
 }
