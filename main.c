f39cdb761f7d07153b9aa9b2ef9749cba6fa498c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-06 23:54:58 -0300

    att

--- main.c
+++ main.c
@@ -2,7 +2,11 @@
 
 #include "CLinEntry/CliEntryMain.h"
 
-int main(){
-    
+int main(int argc, char **argv){
+    CliEntry *e = newCliEntry(argc,argv);
+    double r = CliEntry_get_double(e,1);
+    long r1 =  CliEntry_get_long(e,1);
+
+    printf("%lf",r);
     
 }
