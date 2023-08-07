946c30f3e1c913dff38501e71a53457c4092b9a5
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-07 07:17:07 -0300

    att

--- build.py
+++ build.py
@@ -5,19 +5,20 @@ from Build.full_folder_zip import zip_folder
 
 
 STARTER = 'CliEntry/CliEntryMain.h'
-TEST_NAME  = 'CTextEngine.h'
+TEST_NAME  = 'CliEntryMain_test.h'
 
 OUTPUT_TEST = 'tests/CTextEngine.h'
-OUTPUT = 'CTextEngine.h'
-ZIP_NAME ='CTextEngine'
+OUTPUT = 'CliEntry.h'
+ZIP_NAME ='CliEntry'
 
+'''
 
 ct.generate_amalgamated_code(STARTER,OUTPUT_TEST)
 test = ct.FolderTestPreset(folder='tests/main_test',side_effect_folder='tests/side_effect')
 test.generate_ouptut()
 test.start_test()
 create_exemples(TEST_NAME,OUTPUT)
-
+'''
 ct.include_code_in_markdown('README.md',save_file=True)
 
 ct.generate_amalgamated_code(STARTER,OUTPUT)
