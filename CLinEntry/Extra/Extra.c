f6b3dc3e33f537d8954e9cc8eed2a57a48087277
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-07 02:50:57 -0300

    at

--- CLinEntry/Extra/Extra.c
+++ CLinEntry/Extra/Extra.c
@@ -1,19 +1,30 @@
-void private_cli_parse_flags_sort_text_array(CTextArray *element){
-    
+
+
+
+int private_cli_compare_elements_size(const void *element1,const  void *element2){
+    CTextStack  *s1 = (CTextStack*)element1;
+    CTextStack  *s2 = (CTextStack*)element2;
+    return (int)(s1->size - s2->size);
 }
+
+
+void private_cli_sort_text_arrays_by_size(CTextArray *element){
+    qsort(element, element->size,sizeof(CTextArray*),private_cli_compare_elements_size);
+}
+
 
 CTextArray * private_cli_parse_flags(const char *flags,bool case_sensitive){
     CTextArray *elements = CTextArray_split(flags,"|");
     CTextArray_foreach(elements,CTextStack_self_trim);
-
+    
     if(!case_sensitive){
         CTextArray_foreach(elements,CTextStack_self_lower);
     }
-
+    private_cli_sort_text_arrays_by_size(elements);
     return elements;
 }
 
-CTextStack * private_cli_get_flag_if_its_an_flag(CTextArray *identifiers,CTextStack *possible_flag,bool case_sensitve){
+CTextStack *private_cli_get_flag_if_its_an_flag(CTextArray *identifiers,CTextStack *possible_flag,bool case_sensitve){
 
     CTextStack *formated_possible_flag = CTextStack_clone(possible_flag);
     if(!case_sensitve){
@@ -27,7 +38,7 @@ CTextStack * private_cli_get_flag_if_its_an_flag(CTextArray *identifiers,CTextSt
 
         if(CTextStack_starts_with(formated_possible_flag,current_identifier->rendered_text)){
             CTextStack_self_substr(formated_possible_flag, (long)current_identifier->size,-1);
-
+            
             return formated_possible_flag;
         }
 
--- CLinEntry/Extra/Extra.h
+++ CLinEntry/Extra/Extra.h
@@ -1,5 +1,6 @@
+int private_cli_compare_elements_size(const void *element1,const  void *element2);
 
-void private_cli_parse_flags_sort_text_array(CTextArray *element);
+void private_cli_sort_text_arrays_by_size(CTextArray *element);
 
 CTextArray * private_cli_parse_flags(const char *flags,bool case_sensitive);
 
