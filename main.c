851268d0710f8ba9e71a6fb656c400220841df0f
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-08 19:35:15 -0300

    att

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 
 
 #include "CliEntry/CliEntryMain.h"
-void execute_code(int argc,char **argv){
+
+
+int main(int argc, char **argv){
+
     CliNamespace cli = newCliNamespace();
 
     CliEntry *entry = newCliEntry(argc,argv);
@@ -11,23 +14,25 @@ void execute_code(int argc,char **argv){
     if(entry->size < 2){
         printf("number 1 not exist\n");
         cli.entry.free(entry);
-        return;
+        return 0;
     }
 
     if(!cli.entry.is_numeric(entry,2)){
         printf("number 1 is not an number\n");
         cli.entry.free(entry);
+        return 0;
     }
 
     if(entry->size < 3){
         printf("number 2 not exist\n");
         cli.entry.free(entry);
-        return;
+        return 0;
     }
 
     if(!cli.entry.is_numeric(entry,3)){
         printf("number 2 is not an number\n");
         cli.entry.free(entry);
+        return 0;
     }
     double n1 = cli.entry.get_double(entry,2);
     double n2 = cli.entry.get_double(entry,3);
@@ -36,7 +41,7 @@ void execute_code(int argc,char **argv){
     if(!operation){
         printf("operation not found\n");
         cli.entry.free(entry);
-        return;
+        return 0;
     }
     double  result;
     if(strcmp(operation,"add") == 0){
@@ -45,7 +50,7 @@ void execute_code(int argc,char **argv){
     else if(strcmp(operation,"sub") == 0){
         result = n1-n2;
     }
-    else if(strcmp(operation,"mull") == 0){
+    else if(strcmp(operation,"mul") == 0){
         result = n1*n2;
     }
     else if(strcmp(operation,"div") == 0){
@@ -54,22 +59,10 @@ void execute_code(int argc,char **argv){
     else{
         printf("operation %s is invalid\n",operation);
         cli.entry.free(entry);
-        return;
+        return 0;
     }
     printf("result: %lf\n",result);
-
-}
-
-int main(int argc, char **argv){
-
-    argv = (char*[]){"./a.out","add","20","30"};
-    argc = sizeof(argv) / sizeof(char*);
-    execute_code(argc,argv);
     return 0;
-
-
-
-
 
 
 }
--- tests/main_test/normal_entry/T_calculator/exec.c
+++ tests/main_test/normal_entry/T_calculator/exec.c
@@ -1,13 +1,68 @@
 
 
-#include "../../../CliEntryTest.h"
+#include "CliEntry/CliEntryMain.h"
+
 
 int main(int argc, char **argv){
-    char *entry[]= {"aaa","bbbb"};
-    CliEntry *e = newCliEntry(sizeof(entry) / sizeof(char*),entry);
-    
-    CliEntry_represent(e);
-    CliEntry_free(e);
 
-    
+    CliNamespace cli = newCliNamespace();
+
+    CliEntry *entry = newCliEntry(argc,argv);
+
+
+
+    if(entry->size < 2){
+        printf("number 1 not exist\n");
+        cli.entry.free(entry);
+        return 0;
+    }
+
+    if(!cli.entry.is_numeric(entry,2)){
+        printf("number 1 is not an number\n");
+        cli.entry.free(entry);
+        return 0;
+    }
+
+    if(entry->size < 3){
+        printf("number 2 not exist\n");
+        cli.entry.free(entry);
+        return 0;
+    }
+
+    if(!cli.entry.is_numeric(entry,3)){
+        printf("number 2 is not an number\n");
+        cli.entry.free(entry);
+        return 0;
+    }
+    double n1 = cli.entry.get_double(entry,2);
+    double n2 = cli.entry.get_double(entry,3);
+    char *operation = cli.entry.get_str(entry,1,CLI_NOT_CASE_SENSITIVE);
+
+    if(!operation){
+        printf("operation not found\n");
+        cli.entry.free(entry);
+        return 0;
+    }
+    double  result;
+    if(strcmp(operation,"add") == 0){
+        result = n1+n2;
+    }
+    else if(strcmp(operation,"sub") == 0){
+        result = n1-n2;
+    }
+    else if(strcmp(operation,"mul") == 0){
+        result = n1*n2;
+    }
+    else if(strcmp(operation,"div") == 0){
+        result = n1/n2;
+    }
+    else{
+        printf("operation %s is invalid\n",operation);
+        cli.entry.free(entry);
+        return 0;
+    }
+    printf("result: %lf\n",result);
+    return 0;
+
+
 }
